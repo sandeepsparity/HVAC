1d9a528fb03197eb65151d4c02df641b5837be53
Sandeep R. Mankala <smankala@fxpal.com>
2018-02-22 22:49:32 -0800

    added missing toast file

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import { Root } from "native-base";
 import RootStack from "HVAC/src/config/routing";
 
 export default class App extends React.Component {
   render() {
-    return  <RootStack />
+    return  (<Root>
+      <RootStack />
+      </Root>)
 
   }
 }
